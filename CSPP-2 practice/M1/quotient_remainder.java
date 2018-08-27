32a6dcc70e14940c7dca731d5ca76fcabf1997fa
khyathimsit <khyathi17@msitprogram.net>
2018-08-27 22:35:50 +0530

    Quot commit

---
+++ CSPP-2 practice/M1/quotient_remainder.java
@@ -0,0 +1,10 @@
+public class quotient_remainder{
+	public static void main(String[] args){
+	int a = 15;
+	int b = 4;
+	int rem = a/b;
+	int quot = a%b;
+	System.out.println("rem : "+rem+ " quotent : "+quot);
+
+	}
+}
