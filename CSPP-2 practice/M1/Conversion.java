a4b5ac4a890c74f37898c6b0b4dd0e829c3166fa
khyathimsit <khyathi17@msitprogram.net>
2018-08-27 22:49:46 +0530

    Conversion commit

---
+++ CSPP-2 practice/M1/Conversion.java
@@ -0,0 +1,8 @@
+public class Conversion{
+	public static void main(String[] args) {
+		double fahrenheit;
+		double degreesCelsius = 110.0;
+		fahrenheit = (degreesCelsius*9/5)+32; 
+		System.out.println(fahrenheit);
+	}
+}
