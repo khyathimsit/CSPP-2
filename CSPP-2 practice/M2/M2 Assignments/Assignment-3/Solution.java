2a2dba423fc8c3170867e9d04e1429b1244286bb
khyathimsit <khyathi17@msitprogram.net>
2018-08-28 17:34:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M2/M2 Assignments/Assignment-3/Solution.java
+++ CSPP-2 practice/M2/M2 Assignments/Assignment-3/Solution.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
     * scanner function for taking inputs
 */
 public final class Solution {
-	/**.
+    /**.
         *constructor
     */
     private Solution() {
@@ -17,17 +17,17 @@ public final class Solution {
         * @param args of type string : command line arguments
     */
     public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
-	}
+    }
 
-	/*
-	Need to write the power function and print the output.
-	*/
-	public static long power(final int base, final int exponent) {
+    /*
+    Need to write the power function and print the output.
+    */
+    public static long power(final int base, final int exponent) {
         if (exponent != 0) {
             return (base * power(base, exponent - 1));
         }
