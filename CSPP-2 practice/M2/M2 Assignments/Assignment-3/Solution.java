ff1154f04a42d57bbc62490286b947fba9303479
khyathimsit <khyathi17@msitprogram.net>
2018-08-28 17:15:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ CSPP-2 practice/M2/M2 Assignments/Assignment-3/Solution.java
@@ -0,0 +1,41 @@
+/**
+    * Author : Khyathi.
+    * Date : 27/08/2018
+*/
+import java.util.Scanner;
+/**.
+    * scanner function for taking inputs
+*/
+import java.lang.Math;
+/**.
+    * Math function for calculations
+*/
+
+public class Solution {
+	/**.
+        *constructor
+    */
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result=power(base,exponent);
+        System.out.println(result);
+	}
+
+	/*
+	Need to write the power function and print the output.
+	*/
+	public static long power(int base, int exponent) {
+        if (exponent != 0)
+            return (base * power(base, exponent - 1));
+        else
+            return 1;
+    }
+}
