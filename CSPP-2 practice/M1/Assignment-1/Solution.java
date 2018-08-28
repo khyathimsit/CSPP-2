1594ae26e76e0a516f075b1acf49578664ff8199
khyathimsit <khyathi17@msitprogram.net>
2018-08-28 09:34:37 +0530

    Name-roll commit

---
+++ CSPP-2 practice/M1/Assignment-1/Solution.java
@@ -0,0 +1,26 @@
+
+/**
+    * Author : Khyathi.
+    * Date : 27/08/2018
+*/
+import java.util.Scanner;
+/**.
+    * scanner function for taking inputs
+*/
+public final class Solution {
+    /**.
+        *constructor
+    */
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int rnum = sc.nextInt();
+        System.out.println("Name :" + name + " Roll Number : " + rnum);
+    }
+}
