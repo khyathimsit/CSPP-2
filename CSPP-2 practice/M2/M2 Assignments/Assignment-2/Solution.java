677185c0623780e0e0e9fef408f6ae6f22bc261b
khyathimsit <khyathi17@msitprogram.net>
2018-08-28 18:22:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP-2 practice/M2/M2 Assignments/Assignment-2/Solution.java
+++ CSPP-2 practice/M2/M2 Assignments/Assignment-2/Solution.java
@@ -1,30 +1,48 @@
+/**
+    * Author : Khyathi.
+    * Date : 28/08/2018
+*/
 import java.util.Scanner;
+/**.
+    * scanner function for taking inputs
+*/
 import java.lang.Math;
-
-public class Solution {
-    /*
-    Do not modify this main function.
+/**.
+    * mathematical calculations
+*/
+public final class Solution {
+    /**.
+        *constructor
+    */
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        // float res;
-        rootsOfQuadraticEquation(a, b, c);
-        //System.out.println(res);
+        roots(a, b, c);
     }
     /*
     Need to write the rootsOfQuadraticEquation function and print the output.
     */
-    public static float rootsOfQuadraticEquation(int a, int b, int c)
+    /**
+    * @return float
+    * @param base **this is base number**
+    * @param exponent **this is exponent value**
+    */
+    public static float roots(int a, int b, int c)
     {
         double determinant;
         determinant = (b * b) - (4 * a * c);
         double r1;
         double r2;
-        r1 = (-b+Math.sqrt(determinant))/(2*a);
-        r2 = (-b-Math.sqrt(determinant))/(2*a);
+        r1 = (-b + Math.sqrt(determinant)) / (2*a);
+        r2 = (-b - Math.sqrt(determinant)) / (2*a);
         System.out.println(+r1+" "+r2);
         return 0;
     }
