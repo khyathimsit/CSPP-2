46a038d0af441b389e235bb4bd07186813f42248
khyathimsit <khyathi17@msitprogram.net>
2018-08-29 14:13:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M3/Assignments/Assignment-3/Solution.java
+++ CSPP-2 practice/M3/Assignments/Assignment-3/Solution.java
@@ -1,20 +1,36 @@
-
+/**
+    * Author : Khyathi.
+    * Date : 29/08/2018
+*/
 import java.util.Scanner;
-/*
-    Do not modify this main function.
+/**.
+    * scanner function for taking inputs
+*/
+public final class Solution {
+    /**.
+        *constructor
     */
-public class Solution {
-
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
     public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int x = gcd(n1,n2);
+        int x = gcd(n1, n2);
         System.out.println(x);
     }
     /*
     Need to write the gcd function and print the output.
+    */
+    /**
+    * @return int
+    * @param a **number 1**
+    * @param b **number 2**
     */
     public static int gcd(final int a, final int b) {
        int gcd_val = 0;
