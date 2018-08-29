4cd8f3ffc314d2e5440e42d02f8d9d4d8123f907
khyathimsit <khyathi17@msitprogram.net>
2018-08-29 14:25:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M3/Assignments/Assignment-2/Solution.java
+++ CSPP-2 practice/M3/Assignments/Assignment-2/Solution.java
@@ -1,15 +1,27 @@
+/**
+    * Author : Khyathi.
+    * Date : 29/08/2018
+*/
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
+/**.
+    * scanner function for taking inputs
+*/
+public final class Solution {
+    /**.
+        *constructor
+    */
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int i = 0;
         int cnt = 0;
-        for (i = 1;i <= n;i++) {
+        for (i = 1; i <= n; i++) {
         	int j = i;
         	while (j != 0) {
         		if (j % 10 == 7) {
