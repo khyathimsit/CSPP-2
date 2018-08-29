eb75ac4f30158a342aacf955f4d312989095ff30
khyathimsit <khyathi17@msitprogram.net>
2018-08-29 12:32:25 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M3/Assignments/Assignment-2/Solution.java
+++ CSPP-2 practice/M3/Assignments/Assignment-2/Solution.java
@@ -10,16 +10,13 @@ public class Solution {
         int n = s.nextInt();
         int i = 0;
         int cnt = 0;
-        for(i=1;i<=n;i++)
-        {
+        for(i = 1;i <= n;i++) {
         	int j = i;
-        	while(j!=0)
-        	{
-        		if(j%10==7)
-        		{
-        			cnt = cnt+1;
+        	while(j != 0) {
+        		if(j % 10 ==7) {
+        			cnt = cnt + 1;
         		}
-        		j=j/10;
+        		j = j / 10;
         	}
         }
         System.out.println(cnt);
