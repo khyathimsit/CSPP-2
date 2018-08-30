156376272688cb8ac7e9d9799a10b6e0733c0f07
khyathimsit <khyathi17@msitprogram.net>
2018-08-31 00:08:37 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- CSPP-2 practice/M4/Assignments/Assignment-3/Solution.java
+++ CSPP-2 practice/M4/Assignments/Assignment-3/Solution.java
@@ -8,7 +8,7 @@ public class Solution
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+		int n= Integer.parseInt(sc.nextLine());
 		for(int i=0;i<n;i++){
 			String s=sc.nextLine();
 			long res=binaryToDecimal(s);//Write binaryToDecimal function
@@ -22,7 +22,7 @@ public class Solution
 		int j = 0;
 		for (int i=len-1;i>=0;i--)
 		{
-			sum += Character.getNumericValue(str.charAt(i))* (Math.pow(2,j));
+			sum += Character.getNumericValue(str.charAt(i))* (Math.pow(2, j));
 			j++;
 		}
 		return sum;
