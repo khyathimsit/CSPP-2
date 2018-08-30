54fa8ae5feac3b8fcc30f67050028afe508ab9d4
khyathimsit <khyathi17@msitprogram.net>
2018-08-30 21:37:33 +0530

    Concatinate

---
+++ CSPP-2 practice/M4/Concatinate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Concatinate 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String name =sc.next();
+        System.out.println("Hello " + name + "!"); 
+    }
+}
