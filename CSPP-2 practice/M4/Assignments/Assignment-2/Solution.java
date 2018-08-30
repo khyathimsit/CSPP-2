4f620b634c262a9e85cc203702b313e517208a79
khyathimsit <khyathi17@msitprogram.net>
2018-08-30 14:12:00 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP-2 practice/M4/Assignments/Assignment-2/Solution.java
+++ CSPP-2 practice/M4/Assignments/Assignment-2/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
         Scanner sc = new Scanner(System.in);
         int row1 = sc.nextInt();
         int col1 = sc.nextInt();
-        int mat1[][] = new int[row1][col1];
+        int[][]mat1 = new int[row1][col1];
         for (int c = 0; c < row1; c++) {
             for (int d = 0; d < col1; d++) {
                 mat1[c][d] = sc.nextInt();
@@ -31,14 +31,14 @@ public final class Solution {
 
         int row2 = sc.nextInt();
         int col2 = sc.nextInt();
-        int mat2[][] = new int[row2][col2];
+        int[][]mat2 = new int[row2][col2];
         for (int c = 0; c < row2; c++) {
             for (int d = 0; d < col2; d++) {
                 mat2[c][d] = sc.nextInt();
             }
         }
         if (row1 == row2 && col1 == col2) {
-            int [][]c= new int[row2][col2];
+            int[][]c= new int[row2][col2];
             for (int i = 0; i < row1; i++) {
                 for (int j = 0; j < col2; j++) {
                     c[i][j] = mat1[i][j] + mat2[i][j];
@@ -57,3 +57,4 @@ public final class Solution {
         }
     }
 }
+
