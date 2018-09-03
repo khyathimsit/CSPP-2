d23ba9e91b45ef4f9bcb2bfb98f501743bcf75bd
khyathimsit <khyathi17@msitprogram.net>
2018-09-03 18:16:19 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M7/Assignments/Assignment-1/Solution.java
+++ CSPP-2 practice/M7/Assignments/Assignment-1/Solution.java
@@ -1,15 +1,29 @@
 /**
- * Author: Khyathi
- */
+    * Author : Khyathi.
+    * Date : 3/09/2018
+*/
 import java.util.Scanner;
+/**.
+    * scanner function for taking inputs
+*/
 class InputValidator {
 	/*Write the atrributes and methods for InputValidator*/
 	String data;
 	int length;
     InputValidator(String data, int len) {
+    /**.
+        *constructor
+    */
 	this.data = data;
 	this.length = len;
     }
+    /**
+     * to check the length of the input
+     *
+     * @param      length  The length
+     *
+     * @return     true or false
+     */
     public static boolean validateData(int length) {
     	if (length >= 6) {
     		return true;
@@ -20,8 +34,8 @@ class InputValidator {
 }
 public class Solution {
 	public static void main(String args[]) {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
     	int length = input.length();
     	InputValidator i = new InputValidator(input, length);    	
     	System.out.println(i.validateData(length));
