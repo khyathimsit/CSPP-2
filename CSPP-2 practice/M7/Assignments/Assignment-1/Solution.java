372db928d20c169d5956c4f8d71924f82a8fc5f8
khyathimsit <khyathi17@msitprogram.net>
2018-09-03 18:19:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP-2 practice/M7/Assignments/Assignment-1/Solution.java
+++ CSPP-2 practice/M7/Assignments/Assignment-1/Solution.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
     * scanner function for taking inputs
 */
 class InputValidator {
-	/*Write the atrributes and methods for InputValidator*/
-	String data;
-	int length;
+    /*Write the atrributes and methods for InputValidator*/
+    String data;
+    int length;
     InputValidator(String data, int len) {
     /**.
         *constructor
     */
-	this.data = data;
-	this.length = len;
+    this.data = data;
+    this.length = len;
     }
     /**
      * to check the length of the input
@@ -25,19 +25,29 @@ class InputValidator {
      * @return     true or false
      */
     public static boolean validateData(int length) {
-    	if (length >= 6) {
-    		return true;
-    	} else {
-    		return false;
-    	}
-	}
+        if (length >= 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
-public class Solution {
-	public static void main(String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	int length = input.length();
-    	InputValidator i = new InputValidator(input, length);    	
-    	System.out.println(i.validateData(length));
+public final class Solution {
+    /* Fill the main function to print resultant of addition of matrices*/
+    /**.
+        *constructor
+    */
+    private Solution() {
+        // zero argument constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        int length = input.length();
+        InputValidator i = new InputValidator(input, length);
+        System.out.println(i.validateData(length));
     }
 }
