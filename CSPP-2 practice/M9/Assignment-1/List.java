e45ecf1b859780a77e900e81c1113d18835c50a9
khyathimsit <khyathi17@msitprogram.net>
2018-09-05 11:51:20 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP-2 practice/M9/Assignment-1/List.java
+++ CSPP-2 practice/M9/Assignment-1/List.java
@@ -78,7 +78,10 @@ public class List {
         // That is the initial value to use for size.
         size = 0;
     }
-
+    public List(int n) {
+    	list = new int[10];
+    	size = 0;
+    }
     /*
      * Overloaded constructor with list capacity as argument
      * The default constructor sets the list capacity to 10
