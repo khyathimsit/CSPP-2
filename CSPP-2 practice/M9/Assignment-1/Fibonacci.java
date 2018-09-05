fc1e4a39437a77b7997aedace1c8e04286bb3856
khyathimsit <khyathi17@msitprogram.net>
2018-09-05 11:53:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP-2 practice/M9/Assignment-1/Fibonacci.java
+++ CSPP-2 practice/M9/Assignment-1/Fibonacci.java
@@ -34,7 +34,7 @@ public class Fibonacci
         // 	int n3 = n1 + n2;
         // 	n1 = n2;
         // 	n2 = n3;
-        List fib = new List();
+        List fib = new List(n);
         fib.add(0);
         fib.add(1);
         for(int i = 2; i < n; i++) {
