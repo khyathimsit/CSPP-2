86954b166eb81df8a9dd01585e084607b7bbb6b7
khyathimsit <khyathi17@msitprogram.net>
2018-09-06 17:13:51 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- CSPP-2 practice/M10/Assignment-3/Solution.java
+++ CSPP-2 practice/M10/Assignment-3/Solution.java
@@ -318,8 +318,8 @@ public class Solution {
                         System.out.println(listDouble.size());
                         break;
                         case "print":
-                        // print the list (implement toString in StringList class
-                        //for this to work)
+                        // print the list (implement toString 
+                        // in StringList class for this to work)
                         // expected format is [item-1,item-2,...,item-n]
                         // review the output testcase file
                         System.out.println(listDouble);
@@ -328,16 +328,16 @@ public class Solution {
                         listDouble.remove(Integer.parseInt(tokens[1]));
                         break;
                         case "indexOf":
-                        System.out.println(listDouble.indexOf
-                        (Double.parseDouble(tokens[1])));
+                        System.out.println(listDouble.indexOf(
+                        Double.parseDouble(tokens[1])));
                         break;
                         case "get":
-                        System.out.println(listDouble.get
-                        (Integer.parseInt(tokens[1])));
+                        System.out.println(listDouble.get(
+                        Integer.parseInt(tokens[1])));
                         break;
                         case "contains":
-                        System.out.println(listDouble.contains
-                        (Double.parseDouble(tokens[1])));
+                        System.out.println(listDouble.contains(
+                        Double.parseDouble(tokens[1])));
                         break;
                         default:
                         break;
@@ -345,7 +345,7 @@ public class Solution {
                 }
             break;
 
-            case "O"://This case will be executed for 
+            case "O"://This case will be executed for
             //Student type list i.e to store List of Student Objects
                 List<Student> listStudent = new List();
                 while (stdin.hasNext()) {
@@ -353,7 +353,7 @@ public class Solution {
                     String line = stdin.nextLine();
                     // split the line using space
                     String[] tokens = line.split(" ");
-                    // based on the list operation invoke 
+                    // based on the list operation invoke
                     //the corresponding method
                     switch (tokens[0]) {
                         case "add":
@@ -399,6 +399,6 @@ public class Solution {
                     }
                 }
             break;
-        }    
+        }   
     }
 }
