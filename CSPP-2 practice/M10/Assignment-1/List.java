a07ac43d568211d8fd529a5f6e186b009d23433f
khyathimsit <khyathi17@msitprogram.net>
2018-09-06 12:30:53 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- CSPP-2 practice/M10/Assignment-1/List.java
+++ CSPP-2 practice/M10/Assignment-1/List.java
@@ -105,7 +105,7 @@ public class List {
      * constructor.
      *
      */
-    public List(int capacity) {
+    public List(final int capacity) {
         size = 0;
         list = new int[capacity];
     }
@@ -130,7 +130,7 @@ public class List {
         if (size == list.length) {
             resize();
         }
-        list[size++] = item;   
+        list[size++] = item;
     }
     /*
      *
@@ -145,21 +145,23 @@ public class List {
      * Create a new array of the desired size,
      * and copy the contents from the original array to the new array,
      * using java.lang.System.arraycopy(...);
-     * 
+     *
      * Option 2
      * Use java.util.Arrays.copyOf(...) methods which returns a bigger array,
      * with the contents of the original array.
      *
      * TODO
-     * Create a method called resize(). Resize should create an new array that is
+     * Create a method called resize(). Resize should
+     * create an new array that is
      * double the size of the old array.
      * Then copy the contents of the old array to the new one.
-     * 
+     *
      * When should the resize method be invoked and from where?
      * Will the client invoke resize or is it internal to List class?
      * Should the resize be public method or private?
      * Should the resize method return any values?
-     * You know enough of Object Oriented Programming to answer these questions :-)
+     * You know enough of Object Oriented
+     *Programming to answer these questions :-)
      *
      */
     // todo create resize method
@@ -171,13 +173,16 @@ public class List {
      * The method returns an int. Empty list should return 0.
      */
     public int size() {
+        /**.
+         * { item_description }
+         */
         return size;
     }
     /*
      * The remove method does what the name suggests.
      * Removes an int item, specified by the index argument, from the list
      * It also does an additional step.
-     * Think about what happens when 
+     * Think about what happens when
      * an item is removed from the middle of the list
      * It creates a hole in the list, right?
      * This would mean, all the items that are
@@ -198,11 +203,11 @@ public class List {
      *
      * @param      index  The index
      */
-    public void remove(int index) {
+    public void remove(final int index) {
         // write the logic for remove here.
         // Think about what to do to the size variable.
-        if(index >= 0 && index < size) {
-            for(int i = index; i < size - 1; i++) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size - 1; i++) {
                 list[i] = list[i + 1];
             }
             size--;
@@ -218,7 +223,7 @@ public class List {
      * How can an element not be there at a given position?
      * Well, if the position is greater than the number of items
      * in the list then that would mean the item doesn't exist.
-     * How do we check if the position is greater than the 
+     * How do we check if the position is greater than the
      * number of items in the list? Would size variable be useful?
      */
     /**.
@@ -228,7 +233,7 @@ public class List {
      *
      * @return     { description_of_the_return_value }
      */
-    public int get(int index) {
+    public int get(final int index) {
         if (index < 0 || index >= size) {
             return -1;
         } else {
@@ -261,12 +266,12 @@ public class List {
      * @return     String representation of the object.
      */
     public String toString() {
-        if(size == 0) {
+        if (size == 0) {
             return "[]";
         }
         String str = "[";
         int i = 0;
-        for(i = 0; i < size - 1; i++) {
+        for (i = 0; i < size - 1; i++) {
             str = str + list[i] + ",";
         }
         str = str + list[i] + "]";
@@ -290,7 +295,7 @@ public class List {
     }
 
     /*
-     * Returns the index of the first occurrence 
+     * Returns the index of the first occurrence
      * of the specified element in this list,
      * or -1 if this list does not contain the element.
      */
@@ -309,7 +314,7 @@ public class List {
         }
         return -1;
     }
-   /*Inserts all the elements of specified int 
+   /*Inserts all the elements of specified int
     array to the end of list*/
     /**.
      * Adds all.
@@ -343,7 +348,7 @@ public class List {
         }
         list[index] = item;
         size++;
-    }  
+    }
     /* Returns the count of occurances of a given item in the list*/
     /**.
      * { function_description }
