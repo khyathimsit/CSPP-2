c4e7b061a50301aa6605085b6f901ccbfc2ab182
khyathimsit <khyathi17@msitprogram.net>
2018-09-12 17:58:56 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP-2 practice/M16/bookyourshow/Solution.java
+++ CSPP-2 practice/M16/bookyourshow/Solution.java
@@ -1,9 +1,12 @@
+/**.
+ * { item_description }
+ */
 import java.io.BufferedInputStream;
 /**.
  * { item_description }
  */
 import java.util.Scanner;
-/** .
+/**.
  *  { item_description }
  */
 import java.util.Arrays;
@@ -15,22 +18,57 @@ class Patron {
      * { item_description }
      */
     private String name, phoneno;
+    /**.
+     * Constructs the object.
+     *
+     * @param      nme    The nme
+     * @param      phono  The phono
+     */
     public Patron(String nme, String phono) {
         this.name = nme;
         this.phoneno = phono;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      name  The name
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String Setname(String name) {
         return this.name = name;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      phoneno  The phoneno
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String Setphoneno(String phoneno) {
         return this.phoneno = phoneno;
     }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getname() {
         return name;
     }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getphoneno() {
         return phoneno;
     }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return this.getname() + "," + this.getphoneno();
     }
@@ -39,64 +77,159 @@ class Patron {
  * Class for show.
  */
 class Show {
+    /**.
+     * { var_description }
+     */
     private String movename;
+    /**.
+     * { var_description }
+     */
     private String datetime;
+    /**.
+     * { var_description }
+     */
     private String[] seats;
+    /**.
+     * Constructs the object.
+     *
+     * @param      mvnme   The mvnme
+     * @param      dtetme  The dtetme
+     * @param      seat    The seat
+     */
     public Show(final String mvnme, final String dtetme, final String[] seat) {
         this.movename = mvnme;
         this.datetime = dtetme;
         this.seats = seat;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      mvnme  The mvnme
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String Setmovie(String mvnme) {
         return this.movename = mvnme;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      dtetme  The dtetme
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String Setdatetime(String dtetme) {
         return this.datetime = dtetme;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      seat  The seat
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String[] Setseat(final String[] seat) {
         return this.seats = seat;
     }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getmovie() {
         return this.movename;
     }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String getdatetime() {
         return this.datetime;
     }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String[] getseat() {
         return this.seats;
     }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return this.getmovie() + "," + this.getdatetime();
     }
 }
+/**.
+ * Class for book your show.
+ */
 class BookYourShow {
+    /**.
+     * { var_description }
+     */
     private List<Show> shows;
+    /**.
+     * { var_description }
+     */
     private List<String> tickets;
+    /**.
+     * Constructs the object.
+     */
     public BookYourShow() {
         shows = new List<Show>();
         tickets = new List<String>();
     }
+    /**.
+     * Adds a show.
+     *
+     * @param      s     { parameter_description }
+     */
     public void addAShow(Show s) {
         shows.add(s);
     }
+    /**.
+     * Gets a show.
+     *
+     * @param      movename  The movename
+     * @param      datetime  The datetime
+     *
+     * @return     A show.
+     */
     public Show getAShow(final String movename, final String datetime) {
         for (int i = 0; i < shows.size(); i++) {
             Show show = shows.get(i);
-            if(show.getmovie().equals(movename) && show.getdatetime().equals(datetime)) {
+            if (show.getmovie().equals(movename) &&
+                show.getdatetime().equals(datetime)) {
                 return show;
             }
         }
         return null;
     }
-    public void bookAShow(final String movename, final String datetime, final Patron p, final String[] seats) {
+    /**.
+     * { function_description }
+     *
+     * @param      movename  The movename
+     * @param      datetime  The datetime
+     * @param      p         { parameter_description }
+     * @param      seats     The seats
+     */
+    public void bookAShow(final String movename,
+        final String datetime, final Patron p, final String[] seats) {
+        /**.
+         * { var_description }
+         */
         Show show = getAShow(movename, datetime);
-        if(show == null) {
+        if (show == null) {
             System.out.println("No show");
         } else {
             String[] showseats = show.getseat();
-            for(int i = 0; i < showseats.length; i++) {
-                for(int j = 0; j < seats.length; j++) {
-                    if(seats[j].equals(showseats[i])) {
+            for (int i = 0; i < showseats.length; i++) {
+                for (int j = 0; j < seats.length; j++) {
+                    if (seats[j].equals(showseats[i])) {
                         showseats[i] = "N/A";
                     }
                 }
@@ -108,7 +241,18 @@ class BookYourShow {
             tickets.add(s.toString());
         }
     }
-    public void printTicket(final String movename, final String datetime, final String mobile) {
+    /**.
+     * Method to print the ticket
+     *
+     * @param      movename  The movename
+     * @param      datetime  The datetime
+     * @param      mobile    The mobile
+     */
+    public void printTicket(final String movename,
+        final String datetime, final String mobile) {
+        /**.
+         * { var_description }
+         */
         String ticket = mobile + " " + movename + " " + datetime;
         // for (int i = 0;i < tickets.size(); i++) {
         if (tickets.contains(ticket)) {
@@ -117,8 +261,11 @@ class BookYourShow {
             System.out.println("Invalid");
         }
     }
+    /**.
+     * Shows all
+     */
     public void showAll() {
-        for(int i = 0; i < shows.size(); i++) {
+        for (int i = 0; i < shows.size(); i++) {
             StringBuffer sb = new StringBuffer();
             Show show = shows.get(i);
             sb.append(show + ",[");
@@ -128,13 +275,12 @@ class BookYourShow {
             }
             sb.append(seats[seats.length - 1] + "]");
             System.out.println(sb.toString());
-
-            // System.out.println(show.toString() + "," +
-            //  Arrays.toString(show.getseat()).replace(" ", ""));
         }
     }
 }
-
+/**.
+ * Solution class
+ */
 public final class Solution {
     /**
      * Constructs the object.
