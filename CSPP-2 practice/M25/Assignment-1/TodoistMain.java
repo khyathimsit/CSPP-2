d490bef9d317eafe3f6c1e39e0d1ae062b7c9a9d
khyathimsit <khyathi17@msitprogram.net>
2018-09-22 12:44:23 +0530

    "check_style_score -> 2 of 14 passed. style score: 0/1 "

--- CSPP-2 practice/M25/Assignment-1/TodoistMain.java
+++ CSPP-2 practice/M25/Assignment-1/TodoistMain.java
@@ -46,9 +46,9 @@ class Task {
     }
 
     Task(String title, String name, int time, boolean imp, boolean urgent, String status) throws Exception{
-        if (title.equals("") || title == null)throw new Exception("Invalid status " + status);
+        if (title.equals("") || title == null)throw new Exception("Title not provided");
         if (timeToComplete < 0) throw new Exception("Invalid timeToComplete " + time);
-        if (status == "todo" || status == "done")throw new Exception("Invalid status " + status);
+        if (status != "todo" && status != "done")throw new Exception("Invalid status " + status);
         //             // this.title = title;
         //             // this.timeToComplete = time;
         //             // this.status = status;
