a21ed0a3f95096415df9681f7a02e7d49b46e498
khyathimsit <khyathi17@msitprogram.net>
2018-09-23 10:54:43 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- CSPP-2 practice/M24/Stringmatching/Plagiarism.java
+++ CSPP-2 practice/M24/Stringmatching/Plagiarism.java
@@ -117,9 +117,9 @@ class Plagiarism {
 		String file1 = "";
 		String file2 = "";
 		int space = 0;
-		sb.append("\t      ");
+		sb.append("\t     ");
 		for(int i = 0; i < listFiles.length-1; i++) {
-			sb.append(listFiles[i].getName() + "    ");
+			sb.append(listFiles[i].getName() + "     ");
 		}
 		sb.append(listFiles[listFiles.length - 1].getName() + "\n");
 		for(int i = 0; i < listFiles.length - 1; i++) {
